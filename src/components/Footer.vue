2172a73cb3b9947f03ee67255e5bafac0daa3b97
mekanio20 <mrxyok138@gmail.com>
2025-07-02 19:15:59 +0500

    footer added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,83 +1,89 @@
 <template>
-  <footer class="w-full pt-20 pb-10 text-white bg-custom-gradient-1 px-4 sm:px-0">
+  <footer class="w-full pt-5 text-white bg-custom-gradient-1 px-4 sm:px-0">
     <div class="container-sm sm:container flex flex-col">
       <div class="w-full flex laptop:flex-row flex-col laptop:items-start justify-between my-16">
         <div class="flex flex-col space-y-5">
-          <div class="flex flex-col space-y-6">
-            <p class="font-manrope text-lg text-white w-[90%]">
+          <div class="flex flex-col space-y-20">
+            <p class="font-manrope text-xl text-white w-[80%]">
               Innowasion Çözgütler Bilen Berk Gurluşyk — Geljegi Bilelikde Gurýarys.
             </p>
             <div class="flex items-center space-x-5">
               <a href="#" target="_blank">
                 <div
-                  class="phone:w-9 phone:h-9 w-7 h-7 p-2 rounded-full bg-transparent border flex items-center justify-center cursor-pointer">
+                  class="phone:w-9 phone:h-9 w-7 h-7 p-1 rounded-full bg-transparent border flex items-center justify-center cursor-pointer">
                   <img class="object-cover" v-lazy="`/svgs/facebook.svg`">
                 </div>
               </a>
-
-              <a href="#"
-                target="_blank">
-
+              <a href="#" target="_blank">
                 <div
-                  class="phone:w-9 phone:h-9 w-7 h-7 p-2 rounded-full bg-transparent border flex items-center justify-center cursor-pointer">
+                  class="phone:w-9 phone:h-9 w-7 h-7 p-1 rounded-full bg-transparent border flex items-center justify-center cursor-pointer">
                   <img class="object-cover" v-lazy="`/svgs/instagram.svg`">
                 </div>
               </a>
               <a href="#" target="_blank">
                 <div
-                  class="phone:w-9 phone:h-9 w-7 h-7 p-2 rounded-full bg-transparent border flex items-center justify-center cursor-pointer">
+                  class="phone:w-9 phone:h-9 w-7 h-7 p-1 rounded-full bg-transparent border flex items-center justify-center cursor-pointer">
                   <img class="object-cover" v-lazy="`/svgs/youtube.svg`">
                 </div>
               </a>
 
             </div>
           </div>
-          <div class="flex items-center space-x-3">
-            <p class="font-manrope text-xl text-white">
-              {{ $t('titles.iletisim') }}
+        </div>
+        <div
+          class="laptop:flex laptop:items-start laptop:justify-end grid phone:grid-cols-3 grid-cols-2 gap-y-14 gap-x-32">
+          <div class="flex flex-col space-y-5">
+            <h4 class="font-manrope text-base font-medium text-white/60">Nawigasiýa</h4>
+            <router-link to="/" class="text-white font-manrope font-[300] text-base">{{
+                    $t('links.home') }}</router-link>
+            <router-link to="/about" class="text-white font-manrope font-[300] text-base">{{
+                    $t('links.about') }}</router-link>
+            <router-link to="/works" class="text-white font-manrope font-[300] text-base">{{
+                    $t('links.works') }}</router-link>
+            <router-link to="/services" class="text-white font-manrope font-[300] text-base">{{
+                    $t('links.services') }}</router-link>
+            <router-link to="/contact" class="text-white font-manrope font-[300] text-base">{{
+                    $t('links.contact') }}</router-link>
+          </div>
+          <div class="flex flex-col space-y-10">
+            <div class="flex flex-col space-y-5">
+              <h4 class="font-manrope text-base font-medium text-white/60">Ýazmak üçin</h4>
+              <a href="mailto:info@ezizdoganlar.com" class="font-manrope text-white font-[300] text-base">
+                info@ezizdoganlar.com
+              </a>
+            </div>
+            <div class="flex flex-col space-y-5">
+              <h4 class="font-manrope text-base font-medium text-white/60">Jaň etmek</h4>
+              <a href="tel:+99312753577" class="font-manrope text-white font-[300] text-base">
+                +993 12 75 35 77
+              </a>
+            </div>
+          </div>
+          <div class="flex flex-col space-y-5">
+            <router-link to="/news" class="font-manrope text-base font-medium text-white/60">Salgymyz</router-link>
+            <p class="font-manrope text-base font-[300]">
+              9 Garagum Street
+              Bagtyyarlyk district Ashgabat
+              745202, Turkmenistan
+            </p>
+            <p class="font-manrope text-base font-[300]">
+              (9:00 - 18:00)
             </p>
           </div>
         </div>
-        <div
-          class="w-full laptop:flex laptop:items-start laptop:justify-between grid phone:grid-cols-3 grid-cols-2 gap-y-14 phone:gap-x-32 sm:gap-x-20 gap-x-10">
-          <div class="flex flex-col space-y-5">
-            <h4 class="font-poppins text-base font-bold">{{ $t('links.bizkim') }}</h4>
-            <router-link to="/#hakkimizda" class="text-m_gray_2 font-sans font-normal text-base">{{
-                    $t('titles.hakkimizda') }}</router-link>
-            <router-link to="/about/#degerlerimiz" class="text-m_gray_2 font-sans font-normal text-base">{{
-                    $t('titles.degerlerimiz') }}</router-link>
-            <router-link to="/about/#hikayemiz" class="text-m_gray_2 font-sans font-normal text-base">{{
-                    $t('titles.hikayemiz') }}</router-link>
-            <router-link to="/#faaliyet_alanarimiz" class="text-m_gray_2 font-sans font-normal text-base">{{
-                    $t('titles.faaliyetAlanlarimiz') }}</router-link>
-            <router-link to="/#rakamlarimiz" class="text-m_gray_2 font-sans font-normal text-base">{{
-                    $t('titles.bizimRakamlarimiz') }}</router-link>
-          </div>
-          <div class="flex flex-col space-y-5">
-            <h4 class="font-poppins text-base font-bold">{{ $t('links.hizmetler') }}</h4>
-            <router-link to="/services/security" class="text-m_gray_2 font-sans font-normal text-base">{{
-                    $t('links.guvenlik') }}</router-link>
-            <router-link to="/services/environment" class="text-m_gray_2 font-sans font-normal text-base">{{
-                    $t('links.cevre') }}</router-link>
-            <router-link to="/services/variation" class="text-m_gray_2 font-sans font-normal text-base">{{
-                    $t('links.cesitlilikVeEsitlilik') }}</router-link>
-            <!-- <router-link to="/services/reports" class="text-m_gray_2 font-sans font-normal text-base">{{ $t('links.raporlar') }}</router-link> -->
-            <router-link to="/services/partners" class="text-m_gray_2 font-sans font-normal text-base">{{
-                    $t('links.partnerler') }}</router-link>
-          </div>
-          <div class="flex flex-col space-y-5">
-            <!-- <router-link to="/career" class="font-poppins text-base font-bold">{{ $t('links.kariyer') }}</router-link> -->
-            <router-link to="/news" class="font-poppins text-base font-bold">{{ $t('links.haberler') }}</router-link>
-            <router-link to="/projects" class="font-poppins text-base font-bold">{{ $t('links.projeler')
-              }}</router-link>
-            <router-link to="/productions" class="font-poppins text-base font-bold">{{ $t('links.uretim')
-              }}</router-link>
-          </div>
-        </div>
+      </div>
+      <div class="flex items-center justify-between pb-6">
+        <p class="font-inter text-sm text-white/60">© 2025 Eziz Doganlar. Ähli hukuklar goragly.</p>
+        <p class="font-inter text-sm text-white/60">Made by ParlakNesil</p>
       </div>
       <div
-        class="flex sm:flex-row flex-col sm:space-y-0 space-y-10 sm:items-center items-start justify-between pt-10 pb-5 border-t-2 border-blue-800">
-        <p class="phone:text-lg text-sm">© 2024 All rights reserved</p>
+        class="flex sm:flex-row flex-col sm:space-y-0 space-y-10 sm:items-center items-start justify-between py-5 border-t-2 border-blue-800">
+        <div class="w-[124px] h-[74px]">
+          <img class="w-full h-full object-cover" src="/svgs/doganlar.svg">
+        </div>
+        <h1 class="font-manrope font-medium text-7xl text-white">
+          EZIZ DOGANLAR
+        </h1>
       </div>
     </div>
   </footer>
